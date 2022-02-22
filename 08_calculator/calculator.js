68819378874e4e332ec387c537a23b75e80ea175
Sheishori <sheishori@gmail.com>
2022-02-22 16:06:30 +0100

    Done 08 and 09

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
 const sum = function() {
-	
+  let a = Array.from(arguments).flat();
+  const sumAll = a.reduce((result, current) => result + current, 0);
+  return sumAll;
 };
 
 const multiply = function() {
-
+  let a = Array.from(arguments).flat();
+  const multiplied = a.reduce((result, current) => result * current, 1);
+  return multiplied;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let num = 1;
+  for (i = 1; i <= a; i++) {
+    num *= i;
+  }
+  return num;
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+	let input = string.toLowerCase().replace(/[^a-z]/g, "");
+	let reversed = input.split("")
+	                    .reverse()
+											.join("");
+	return input === reversed;
 };
 
 // Do not edit below this line
