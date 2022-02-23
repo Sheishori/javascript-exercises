14eafaba46c6de7520a8724b9e2ad2a332b20e78
Sheishori <sheishori@gmail.com>
2022-02-23 12:27:48 +0100

    Done 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+	let sorted = people.sort((first, second) => {
+		let firstAge = (first.yearOfDeath ? first.yearOfDeath : new Date().getFullYear()) - first.yearOfBirth;
+		let secondAge = (second.yearOfDeath ? second.yearOfDeath : new Date().getFullYear()) - second.yearOfBirth;
+		return firstAge > secondAge ? -1 : 1});
+	return sorted[0];
 };
 
 // Do not edit below this line
